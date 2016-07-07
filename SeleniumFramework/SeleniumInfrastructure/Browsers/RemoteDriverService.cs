18644f93836da8a72c3da7f1b578902f718e6432
Ilia Galperin <galperinjob@gmail.com>
2016-07-07 19:11:08 +1200

    Fix remote driver example

--- SeleniumFramework/SeleniumInfrastructure/Browsers/RemoteDriverService.cs
+++ SeleniumFramework/SeleniumInfrastructure/Browsers/RemoteDriverService.cs
@@ -10,12 +10,12 @@ namespace SeleniumFramework.SeleniumInfrastructure.Browsers
         {
             switch (browser)
             {
-                case "RemoteFirefox":
-                    //TO DO: create separate service for Selenium Grid Drivers, this is just example
-                    DesiredCapabilities capabilites = new DesiredCapabilities();
-                    Environment.SetEnvironmentVariable("webdriver.gecko.driver", @"C:\");
-                    capabilites.SetCapability("marionette", true);
-                    return new RemoteWebDriver(new Uri("http://172.17.16.45:5555/wd/hub"), capabilites);
+                case "Firefox":
+                    //For demo, works only for F46
+                    //register Hub - java -jar selenium-server-standalone-2.53.1.jar -role hub
+                    //register Node - java -jar selenium-server-standalone-2.53.1.jar -role webdriver -hub http://192.168.1.6:4444/grid/register
+                    DesiredCapabilities capabilites = DesiredCapabilities.Firefox();
+                    return new RemoteWebDriver(new Uri("http://192.168.1.6:4444/wd/hub"), capabilites);
                 default:
                     throw new ArgumentException(browser + "- Not supported browser");
             }
